c21843c852697fc0801d05fe509c301b2232f11e
Rodrigo Mesquita <rodrigoriome@gmail.com>
2020-08-10 00:18:42 -0300

    Initial commit

---
+++ type-app-react/precache-manifest.2d68b12f69ae8c5a49362b3feb695660.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f3c66837a86ea6d56b7be3ad39e811",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ed90ad16a222d1e11616",
+    "url": "./static/css/main.48131bda.chunk.css"
+  },
+  {
+    "revision": "c33f6274e35980ba844e",
+    "url": "./static/js/2.f84dce68.chunk.js"
+  },
+  {
+    "revision": "ed90ad16a222d1e11616",
+    "url": "./static/js/main.fc2f59da.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
